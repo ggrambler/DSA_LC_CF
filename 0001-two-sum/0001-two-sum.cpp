28f1fe8420266f3f0ebfcb9c712779d2e6bd5f50
Divyansh Bhardwaj urf: ggrambler <83190537+ggrambler@users.noreply.github.com>
2026-06-28 17:03:38 +0530

    Time: 13 ms (36.47%), Space: 18.5 MB (5.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
+        int n = nums.size(), idx2 = 0;
         map <int,int> mapp;
-        int idx = 0,idx2 = 0;
 
         for(int i=0;i<n;i++){
             if(mapp[target-nums[i]]>0){
                 idx2 = i;
                 break;
             }
-            mapp[nums[i]]++;
+            mapp[nums[i]] = i;
         }
-        for(int i=0;i<idx2;i++)if(nums[i] == target - nums[idx2]) idx = i;
         
-        return {idx,idx2};
+        return {mapp[target - nums[idx2]],idx2};
     }
 };
