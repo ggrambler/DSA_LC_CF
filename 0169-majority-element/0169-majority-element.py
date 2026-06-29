abc956e2dc264127e7db1edb311bfd8bd67f9649
Divyansh Bhardwaj urf: ggrambler <83190537+ggrambler@users.noreply.github.com>
2026-06-29 16:07:34 +0530

    Time: 5 ms (65.01%), Space: 21.3 MB (19.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,16 @@
 class Solution:
     def majorityElement(self, nums):
-        counts = collections.Counter(nums)
-        return max(counts.keys(), key=counts.get)
-        
+        # boyer moore
+        count = 0
+        candidate = nums[0]
+
+        for num in nums:
+            if count==0:
+                candidate = num
+                count+=1
+            elif num==candidate:
+                count+=1
+            else:
+                count-=1
+
+        return candidate
