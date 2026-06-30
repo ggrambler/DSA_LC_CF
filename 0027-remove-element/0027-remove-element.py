dbb7ecfb5f254bc2808dedd701ea9f6d5332d2cc
Divyansh Bhardwaj urf: ggrambler <83190537+ggrambler@users.noreply.github.com>
2026-06-30 20:10:59 +0530

    Time: 0 ms (100%), Space: 19.4 MB (17.65%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,24 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        count = sum([1 if x==val else 0 for x in nums])
+        n = len(nums)
+
+        i = 0
+        l = 0
+        r = n-1
+
+        while l<r:
+            while not nums[l]==val:
+                l+=1
+                if l>=n: break
+            while nums[r]==val and r>-1:
+                r-=1
+                if r<=-1: break
+            if l<r:
+                nums[l],nums[r] = nums[r],nums[l]
+                print('swapped',nums)
+
+
+        return n-count
+        
+        
