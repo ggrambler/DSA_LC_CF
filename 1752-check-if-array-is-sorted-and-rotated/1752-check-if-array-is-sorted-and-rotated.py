25573169b6a398e2288413d4306456b35823b5b2
Divyansh Bhardwaj urf: ggrambler <83190537+ggrambler@users.noreply.github.com>
2026-07-02 17:56:39 +0530

    Time: 0 ms (100%), Space: 19.2 MB (52.87%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -6,10 +6,8 @@ class Solution:
         for i in range(n-1):
             if not nums[i]<=nums[i+1]:
                 drop+=1
-        if drop==0:
+                if drop>1:
+                    return False
+        if drop==0 or (drop==1 and nums[n-1]<=nums[0]):
             return True
-        if drop==1 and nums[n-1]<=nums[0]:
-            return True
-        else:
-            return False
-        
+        return False
