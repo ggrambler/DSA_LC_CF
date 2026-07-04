dc6875d5a602405718b50dcef1c5fbb85215c2f7
Divyansh Bhardwaj urf: ggrambler <83190537+ggrambler@users.noreply.github.com>
2026-07-04 15:01:47 +0530

    Time: 114 ms (6.97%), Space: 23 MB (99.31%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -2,9 +2,9 @@ class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         ans = 0
         n = len(nums)
-        base = 0
-        le = [-1 for i in range(n+1)]
-        ue = [-1 for i in range(n+1)]
+        base = 0 # count / total sum 
+        le = [-1 for i in range(n+1)]  # for storing ower bound for each sum
+        ue = [-1 for i in range(n+1)]   # for upper bound of each sum
         le[0] = 0
 
         for i in range(n):
@@ -13,6 +13,7 @@ class Solution:
                 le[base] = i+1
             elif le[base]!=-1:
                 ue[base] = i+1
+
             ans = max(ans,ue[base]-le[base])
             
         return ans     
