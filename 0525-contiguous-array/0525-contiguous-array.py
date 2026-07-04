b57868a917972c0912e51d2fb59f08c7f8717616
Divyansh Bhardwaj urf: ggrambler <83190537+ggrambler@users.noreply.github.com>
2026-07-04 14:57:23 +0530

    Time: 95 ms (19.51%), Space: 23 MB (99.31%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        ans = 0
+        n = len(nums)
+        base = 0
+        le = [-1 for i in range(n+1)]
+        ue = [-1 for i in range(n+1)]
+        le[0] = 0
+
+        for i in range(n):
+            base = base-1 if nums[i]==0 else base+1
+            if le[base]==-1:
+                le[base] = i+1
+            elif le[base]!=-1:
+                ue[base] = i+1
+
+        for i in range(n+1):
+            if ue[i]!=-1 and le[i]!=-1:
+                ans = max(ans,ue[i]-le[i])
+        
+        return ans     
