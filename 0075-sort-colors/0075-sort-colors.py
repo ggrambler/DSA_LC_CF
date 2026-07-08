1bc2b8c32113f97a155173a15981c6e82e963060
Divyansh Bhardwaj urf: ggrambler <83190537+ggrambler@users.noreply.github.com>
2026-07-08 14:12:26 +0530

    Time: 0 ms (100%), Space: 19.3 MB (21.33%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,12 +1,19 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        d = defaultdict(int)
-        for x in nums:
-            d[x]+=1
-        i = 0
-        for val in [0,1,2]:
-            for x in range(d[val]):
-                nums[i] = val
-                i+=1
+        n = len(nums)
 
+        p0 = 0
+        curr = 0
+        p2 = n-1
+
+        while curr<=p2:
+            if nums[curr]==0:
+                nums[curr],nums[p0] = nums[p0],nums[curr]
+                p0+=1
+                curr+=1
+            elif nums[curr]==2:
+                nums[curr],nums[p2] = nums[p2],nums[curr]
+                p2-=1
+            else:
+                curr+=1
         
