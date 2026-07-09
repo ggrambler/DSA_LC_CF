f224e9dda31e78bbcabfa3c1a46360440777b2c3
Divyansh Bhardwaj urf: ggrambler <83190537+ggrambler@users.noreply.github.com>
2026-07-09 23:49:02 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.21%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        #mirror on y axis
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j],matrix[i][n-j-1] = matrix[i][n-j-1],matrix[i][j]
+        #mirror diagonally
+        for i in range(n):
+            for j in range(n-i):
+                matrix[i][j],matrix[n-j-1][n-i-1] = matrix[n-j-1][n-i-1],matrix[i][j]
+        
