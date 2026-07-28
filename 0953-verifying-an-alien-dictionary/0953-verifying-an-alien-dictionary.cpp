ee98354817047798c86113db058163b81a00804c
Divyansh Bhardwaj urf: ggrambler <83190537+ggrambler@users.noreply.github.com>
2026-07-29 00:11:57 +0530

    Time: 0 ms (100%), Space: 12.5 MB (42.25%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    bool cmp(map <char,int>& val, string worda, string wordb){
+        int lena = worda.size();
+        int lenb = wordb.size();
+        int min = (lena<lenb)?lena:lenb;
+
+        int start = 0;
+        while(start<min){
+            if(val[worda[start]]==val[wordb[start]])start++;
+            else if(val[worda[start]]>val[wordb[start]])return true;
+            else return false;
+        }
+        if(lena>lenb) return false;
+        return true;
+    }
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        
+        map <char,int> val;
+        int v = 0;
+        for(char ch:order)val[ch] = 26-v++;
+
+        int n = words.size();
+
+        for(int i=0;i<n-1;i++){
+            if (!cmp(val,words[i],words[i+1])){
+                return false;
+            }
+        }
+        return true;
+    }
+};
