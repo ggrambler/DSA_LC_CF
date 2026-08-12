225ee547411ebc54121299a670e7bc146f2e546f
Divyansh Bhardwaj urf: ggrambler <83190537+ggrambler@users.noreply.github.com>
2026-08-13 00:51:53 +0530

    Time: 19 ms (10.7%), Space: 35.3 MB (7.91%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,22 +1,6 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        k %= n
-
-        count = 0
-        start = 0
-
-        while count < n:
-            curr = start
-            stored = nums[start]
-            while True:
-                nxt = (curr + k) % n
-                nums[nxt], stored = stored, nums[nxt]
-                curr = nxt
-                count += 1
-                # print(curr,stored,nums)
-
-                if curr == start:
-                    break
-
-            start += 1
+        k = len(nums) - k%len(nums)
+        nums[:] = nums[k:] + nums[:k]
+        
+        
