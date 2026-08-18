037ee9676a8a99ec7c24d6225cdacce31258dd6c
Divyansh Bhardwaj urf: ggrambler <83190537+ggrambler@users.noreply.github.com>
2026-08-19 00:40:10 +0530

    Time: 123 ms (5.01%), Space: 39.4 MB (14.04%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -2,12 +2,9 @@ class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         mapp = defaultdict(int)
         n = len(nums)
-        if n==1:
-            return False
 
         for i in range(min(n,k+1)):
             mapp[nums[i]]+=1
-            # print(mapp)
             if mapp[nums[i]]>1:
                 return True
 
